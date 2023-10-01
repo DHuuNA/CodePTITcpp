ebb1fbaaffc4a5194363150a82d8d4ebc4a46347
DHuuNA <134215814+DHuuNA@users.noreply.github.com>
2023-10-01 16:33:23 +0700

    Add files via upload

---
+++ CPP0428.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m;
+		cin>>n>>m;
+		long long a[n+m];
+		for(int i=0;i<m+n;i++){
+			cin>>a[i];
+		}
+		sort(a,a+(m+n));
+		for(int i=0;i<m+n;i++){
+			cout<<a[i]<<" ";
+		}
+		cout<<endl;
+	}
+}
